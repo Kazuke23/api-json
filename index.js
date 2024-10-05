ffc912873e5abbd8504d9969dcf2e622ba781921
Sebas B <sebastian.buitrago01@unicatolica.edu.co>
2024-10-05 10:53:48 -0500

    Modifique el vercel y el index

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const {urlencoded, json} = require('express');
+const { urlencoded, json } = require('express');
 const router = require('./routes/signos.routes.js');
 const cors = require('cors');
 
 const app = express();
 
-app.use(urlencoded({extended: true}))
-app.use(json())
-
-app.use(cors())
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cors());
 
 // Responder a las solicitudes preflight OPTIONS
 app.options('*', cors());
 
+// Usar el router
 app.use('/v1/signos', router);
 
+// Si deseas agregar una ruta para la raíz
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API de Signos');
+});
+
+// Exportar la aplicación
 module.exports = app;
